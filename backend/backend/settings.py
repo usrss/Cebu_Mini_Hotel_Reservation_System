d97e5de180c5d6d6c5c824cd43741f670e2902e3
usrss <bradicarcasona16@gmail.com>
2026-05-09 16:35:56 +0800

    changed the port to 465

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,9 +80,9 @@ CLOUDINARY_STORAGE = {
 # ── Email ─────────────────────────────────────────────────────────────────────
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_PORT          = 587
-EMAIL_USE_TLS       = True
-EMAIL_USE_SSL       = False
+EMAIL_PORT          = 465
+EMAIL_USE_TLS       = False
+EMAIL_USE_SSL       = True
 EMAIL_HOST_USER     = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL  = config('EMAIL_HOST_USER')
